a26caec90979192f2aa0bde6990f23812dcc6b55
Isaac Pinheiro <isaacpnhr@gmail.com>
2016-07-02 17:40:42 -0300

    Writing tests.

--- test/cpf_validator_test.exs
+++ test/cpf_validator_test.exs
@@ -2,7 +2,20 @@ defmodule CpfValidatorTest do
   use ExUnit.Case
   doctest CpfValidator
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "Test valid CPFs" do
+    assert CpfValidator.Validator.validate("111.222.333/92") == true
+    assert CpfValidator.Validator.validate("222.333.444/05") == true
+    assert CpfValidator.Validator.validate("333.444.555/08") == true
+    assert CpfValidator.Validator.validate("123.456.789/09") == true
+    assert CpfValidator.Validator.validate("987.654.321/00") == true
   end
+
+  test "Test invalid CPFs" do
+    assert CpfValidator.Validator.validate("111.222.333/44") == false
+    assert CpfValidator.Validator.validate("222.333.444/55") == false
+    assert CpfValidator.Validator.validate("333.444.555/66") == false
+    assert CpfValidator.Validator.validate("123.456.789/00") == false
+    assert CpfValidator.Validator.validate("987.654.321/11") == false
+  end
+
 end
